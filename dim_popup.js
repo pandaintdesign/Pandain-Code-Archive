6d555cbe87301a11cd744b05bf3a266cfbe7451f
pandaintdesign <pandaintdesign@gmail.com>
2025-01-16 14:32:56 +0900

    메인 index 팝업 html 코드 백업

--- dim_popup.js
+++ dim_popup.js
@@ -1,34 +1,39 @@
-var dim_popup = dim_getCookie('dim_popup')
-if (dim_popup == "" || dim_popup == null ) {
-    jQuery_bs("#dim_popup").addClass('open');
-}        
-    
-jQuery_bs("#dim_popup_close").click(function(){
-    jQuery_bs("#dim_popup").removeClass('open');
-});
-jQuery_bs("#dim_popup_today").click(function(){
-    dim_setCookie('dim_popup', '1', 0, '/');
-    jQuery_bs("#dim_popup").removeClass('open');
-});
 
-function dim_getCookie( name ){
-var dc = document.cookie;
-var prefix = name + "="
-var begin = dc.indexOf("; " + prefix);
-if ( begin == -1 ){
-    begin = dc.indexOf(prefix);
-    if (begin != 0) return null;
+
+
+
+function closePopup(){
+    const closePopup = document.querySelectorAll('.hidePopup');
+    const popupArea = document.getElementById('dim_popup');
+
+    if(closePopup.addEventListner('click')){
+        popupArea.addClass('open');
+    }else{
+        popupArea.removeClass('open');
+    }
+       
 }
-else begin += 2
-var end = document.cookie.indexOf(";", begin);
-if (end == -1) end = dc.length;
-return unescape(dc.substring(begin + prefix.length, end));
-}
-function dim_setCookie( name, value, expires, path, domain, secure ){
-var curCookie = name + "=" + escape( value ) +
-    ( ( expires ) ? "; expires=" + expires.toGMTString() : "" ) +
-    ( ( path ) ? "; path=" + path : "" ) +
-    ( ( domain ) ? "; domain=" + domain : "" ) +
-    ( ( secure ) ? "; secure" : "" );
-document.cookie = curCookie;
-}
+
+
+
+
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView : 1,  //초기값 설정 모바일값이 먼저!!
+        loop: true,
+        spaceBetween: 20,
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    
+        breakpoints: {
+            768:{
+                slidesPerView: 1,  //브라우저가 768보다 클 때
+                spaceBetween: 10,
+            },
+        },
+        autoplay: {
+        	delay: 4000,
+        }
+});
